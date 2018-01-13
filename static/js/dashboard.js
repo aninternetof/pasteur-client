bfb9adc0d1bc234907af935569141ee44eb08ba7
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-01-13 09:44:59 -0500

    Load current state at start

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -27,7 +27,15 @@ $( document ).ready(function() {
     });
 
     $( "#goButton").click(function() {
+        payload = {name: $( "#inputRunName ").val()};
         console.log("Starting run");
+        $.ajax({
+            url:raspAddr+"/api/v1/run",
+            type:"POST",
+            data:JSON.stringify(payload),
+            contentType:"application/json; charset=utf-8",
+            dataType:"json",
+        });
         payload = {value: true};
         $.ajax({
             url:raspAddr+"/api/v1/enabled",
@@ -75,8 +83,20 @@ function postSetup() {
         console.log(msg);
         data = JSON.parse(msg);
         $( "#inputCurrentTemp" ).val(data.temp_reading_degc);
+        if (data.enabled){
+            $( "#inputRunName" ).val(data.name);
+        }
         setEnabledState(data.enabled);
     });
+
+    $.getJSON(raspAddr+"/api/v1/target-temp-degc", function(result){
+        $( "#inputTargetTemp ").val(result.value)
+    });
+    $.getJSON(raspAddr+"/api/v1/target-degc-minutes", function(result){
+        $( "#inputTargetTempMinutes ").val(result.value)
+    });
+
+    
     $('#loginModal').modal('hide');
 }
 
@@ -84,9 +104,12 @@ function setEnabledState(isRunning) {
     if (isRunning) {
         $( "#cancelButton" ).show()
         $( "#goButton" ).hide()
+        $( '#inputRunName' ).prop('readonly', true);
     } else {
         $( "#cancelButton" ).hide()
         $( "#goButton" ).show()
+        $( '#inputRunName' ).prop('readonly', false);
+        $( "#inputRunName" ).val("");
     }
 }
 
